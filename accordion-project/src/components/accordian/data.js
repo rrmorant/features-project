be2617f2f1ab0df896620d51fee2ed915c2b4502
rrmorant <duecel@icloud.com>
2024-02-10 16:45:47 -0800

    Working functionality for accordion component

---
+++ accordion-project/src/components/accordian/data.js
@@ -0,0 +1,28 @@
+const data = [
+	{
+		id: "1",
+		question: "What are accordion components?",
+		answer:
+			"Accordion components are a way to present content in a collapsible, space-saving way. They are often used to present a large amount of information in a limited space.",
+	},
+	{
+		id: "2",
+		question: "What are the benefits of using accordion components?",
+		answer:
+			"Accordion components are a great way to present a large amount of information in a limited space. They are also a great way to organize content and make it easier for users to find what they are looking for.",
+	},
+	{
+		id: "3",
+		question: "How do I use accordion components?",
+		answer:
+			"Accordion components are easy to use. Simply add the component to your page and add the content you want to display in the accordion. You can also customize the appearance of the accordion to match the look and feel of your website.",
+	},
+	{
+		id: "4",
+		question: "What are some best practices for using accordion components?",
+		answer:
+			"When using accordion components, it is important to make sure that the content is easy to read and understand. It is also important to make sure that the accordion is easy to use and navigate.",
+	},
+];
+
+export default data;
